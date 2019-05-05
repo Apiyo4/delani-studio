68940e850806bcedc7a46d93398064039e9029a8
Annette Odingo <odingoannette@gmail.com>
2019-05-05 13:48:51 +0300

    Indent HTML, Javascript and CSS files

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,46 @@
-$(document).ready(function(){
-  $("#one").click(function(event){
+$(document).ready(function() {
+  $("#one").click(function(event) {
     $(".p1").toggle();
     event.preventDefault();
   });
-  $("#two").click(function(event){
+  $("#two").click(function(event) {
     $(".p2").toggle();
     event.preventDefault();
   });
-  $("#three").click(function(event){
+  $("#three").click(function(event) {
     $(".p3").toggle();
     event.preventDefault();
   });
-  $("#img4").hover(function(event){
-   $("#p4").toggle();
-  event.preventDefault();
-});
-$("#img5").hover(function(event){
- $("#p5").toggle();
-event.preventDefault();
-});
-$("#img6").hover(function(event){
- $("#p6").toggle();
-event.preventDefault();
-});
-$("#img7").hover(function(event){
- $("#p7").toggle();
-event.preventDefault();
-});
-$("#img8").hover(function(event){
- $("#p8").toggle();
-event.preventDefault();
-});
-$("#img9").hover(function(event){
- $("#p9").toggle();
-event.preventDefault();
-});
-$("#img10").hover(function(event){
- $("#p10").toggle();
-event.preventDefault();
-});
-$("#img11").hover(function(event){
- $("#p11").toggle();
-event.preventDefault();
-});
+  $("#img4").hover(function(event) {
+    $("#p4").toggle();
+    event.preventDefault();
+  });
+  $("#img5").hover(function(event) {
+    $("#p5").toggle();
+    event.preventDefault();
+  });
+  $("#img6").hover(function(event) {
+    $("#p6").toggle();
+    event.preventDefault();
+  });
+  $("#img7").hover(function(event) {
+    $("#p7").toggle();
+    event.preventDefault();
+  });
+  $("#img8").hover(function(event) {
+    $("#p8").toggle();
+    event.preventDefault();
+  });
+  $("#img9").hover(function(event) {
+    $("#p9").toggle();
+    event.preventDefault();
+  });
+  $("#img10").hover(function(event) {
+    $("#p10").toggle();
+    event.preventDefault();
+  });
+  $("#img11").hover(function(event) {
+    $("#p11").toggle();
+    event.preventDefault();
+  });
 });
