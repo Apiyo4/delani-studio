26d0e54593a96f7ab860f965965cc76d38405a31
Annette Odingo <odingoannette@gmail.com>
2019-05-05 13:05:12 +0300

    Add footer, p  and img elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,46 @@
 $(document).ready(function(){
   $("#one").click(function(event){
-    $(".p1").show();
-    $("#one").click(function(){
-      $(".p1").hide();
+    $(".p1").toggle();
     event.preventDefault();
-  });
   });
   $("#two").click(function(event){
-    $(".p2").show();
-    $("#two").click(function(){
-      $(".p2").hide();
+    $(".p2").toggle();
     event.preventDefault();
-  });
   });
   $("#three").click(function(event){
-    $(".p3").show();
-    $("#three").click(function(){
-      $(".p3").hide();
+    $(".p3").toggle();
     event.preventDefault();
   });
-  });
   $("#img4").hover(function(event){
-   $("#p4").show();
+   $("#p4").toggle();
   event.preventDefault();
+});
+$("#img5").hover(function(event){
+ $("#p5").toggle();
+event.preventDefault();
+});
+$("#img6").hover(function(event){
+ $("#p6").toggle();
+event.preventDefault();
+});
+$("#img7").hover(function(event){
+ $("#p7").toggle();
+event.preventDefault();
+});
+$("#img8").hover(function(event){
+ $("#p8").toggle();
+event.preventDefault();
+});
+$("#img9").hover(function(event){
+ $("#p9").toggle();
+event.preventDefault();
+});
+$("#img10").hover(function(event){
+ $("#p10").toggle();
+event.preventDefault();
+});
+$("#img11").hover(function(event){
+ $("#p11").toggle();
+event.preventDefault();
 });
 });
